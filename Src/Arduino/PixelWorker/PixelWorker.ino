a071e363a149545384f1617f186066e6dc313018
Peon <muratkaya@github.com>
2018-02-11 20:53:54 +0300

    Lib Change

--- Src/Arduino/PixelWorker/PixelWorker.ino
+++ Src/Arduino/PixelWorker/PixelWorker.ino
@@ -1,46 +1,40 @@
-#include "PCF8814.h"
+#include "Nokia2.h"
 #include <math.h>
 
-static PCF8814 lcd; //Nokia 1100 Lib
 int incomingByte = 0; //Serial read
-int currentAngle, oldAngle; //Position markers
+static int currentAngle = 0;
+static int oldAngle = 0; //Position markers
 
+static LCD lcd; //Nokia 1100 Lib
 void setup() {
-  lcd.begin();
-  //lcd.rotate(true);
-  Serial.begin(9600);
-
-  //Initial drawings
-  //Circle - x,y,radius,fill,black/white
-  lcd.lcd_circle(48, 32, 30, 0, 1);
-
-  //Line - x1,y1,x2,y2,black/white
-  lcd.lcd_line(18, 33, 78, 33, 1);
-
-  //Cursor position - x,7
-  lcd.setCursor(0,0);
-  //Print some strings
-  lcd.print("UP");
-    //Cursor position - x,7
-  lcd.setCursor(0,8);
-  lcd.print("DOWN");
-  
+  lcd.Setup();
+  lcd.Clear();
+lcd.print("SKY");
+//void LCD::drawCircleHelper( int16_t x0, int16_t y0,int16_t r, uint8_t cornername, uint16_t color)
+lcd.drawCircle(48,32,30,1);
+lcd.drawLine(18, 33, 78, 33, 1);
+lcd.drawTriangle(44, 33,48, 33,44, 28, 1); 
+lcd.setCursor(0,58);
+lcd.print("GND");
+lcd.Update();
+Serial.begin(9600);
 }
 
 void loop() {
-
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-    if (currentAngle != incomingByte) {
-      lcd.lcd_circle(48, 32, 30, 0, 1);
-      lcd.lcd_line(18, 33, 78, 33, 1);
+    if(true==true){
+    if (oldAngle != incomingByte) {
+      lcd.drawCircle(48,32,30,1);
+      lcd.drawLine(18, 33, 78, 33, 1);
 
       currentAngle = incomingByte;
       lcd.drawAxisLines(oldAngle, 0);
       lcd.drawAxisLines(currentAngle, 1);
       oldAngle = currentAngle;
+      lcd.Update();
+    }
     }
   }
 }
-
